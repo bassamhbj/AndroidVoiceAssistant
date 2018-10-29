df1cd136fef0f6a7fe736d050bf898d2e89f8981
bassamhbj <hbjproduction@gmail.com>
2018-10-30 01:05:35 +0900

    Add Language Settings

--- app/src/main/java/com/hbjpro/androidvoiceassistant/MainActivity.kt
+++ app/src/main/java/com/hbjpro/androidvoiceassistant/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.hbjpro.androidvoiceassistant
 
 import android.Manifest
+import android.content.Intent
+import android.content.SharedPreferences
 import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -27,6 +30,12 @@ class MainActivity : AppCompatActivity(), MainViewPresent.MainViewListener  {
         button1.setOnClickListener(View.OnClickListener {
             _presenter.doSpeechRecognition(Tools.LanguageCode.ENGLISH_AMERICA)
         })
+
+        //PreferenceManager.setDefaultValues(this, R.xml.preferences, false)
+
+        var sharedPreferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        var languageOption = sharedPreferences.getString("list_languages", "-1")
+        setText(languageOption)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -37,6 +46,8 @@ class MainActivity : AppCompatActivity(), MainViewPresent.MainViewListener  {
     override fun onOptionsItemSelected(item: MenuItem?) = when (item?.itemId){
         R.id.action_settings ->{
             setText("Settings press")
+            var intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
             true
         }
         else -> {
